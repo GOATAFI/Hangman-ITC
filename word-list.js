f2e12fff6e3e610b807f2b3300339cb0178d682c
GOATAFI <projectexecutiveofpm@gmail.com>
2023-08-26 08:43:44 +0600

    word updated

--- word-list.js
+++ word-list.js
@@ -32,14 +32,6 @@ const wordList = [
         hint: "An insect with colorful wings and a slender body."
     },
     {
-        word: "history",
-        hint: "The study of past events and human civilization."
-    },
-    {
-        word: "pizza",
-        hint: "A savory dish consisting of a round, flattened base with toppings."
-    },
-    {
         word: "jazz",
         hint: "A genre of music characterized by improvisation and syncopation."
     },
@@ -62,14 +54,6 @@ const wordList = [
     {
         word: "bicycle",
         hint: "A human-powered vehicle with two wheels."
-    },
-    {
-        word: "sunset",
-        hint: "The daily disappearance of the sun below the horizon."
-    },
-    {
-        word: "coffee",
-        hint: "A popular caffeinated beverage made from roasted coffee beans."
     },
     {
         word: "dance",
@@ -144,14 +128,6 @@ const wordList = [
         hint: "A building or outdoor area in which plays, movies, or other performances are staged."
     },
     {
-        word: "telephone",
-        hint: "A device used to transmit sound over long distances."
-    },
-    {
-        word: "language",
-        hint: "A system of communication consisting of words, gestures, and syntax."
-    },
-    {
         word: "desert",
         hint: "A barren or arid land with little or no precipitation."
     },
@@ -188,10 +164,6 @@ const wordList = [
         hint: "A celestial body that orbits a star and does not produce light of its own."
     },
     {
-        word: "river",
-        hint: "A large natural stream of water flowing in a channel to the sea, a lake, or another such stream."
-    },
-    {
         word: "tropical",
         hint: "Relating to or situated in the region between the Tropic of Cancer and the Tropic of Capricorn."
     },
@@ -216,14 +188,6 @@ const wordList = [
         hint: "To speak very softly or quietly, often in a secretive manner."
     },
     {
-        word: "shadow",
-        hint: "A dark area or shape produced by an object blocking the light."
-    },
-    {
-        word: "secret",
-        hint: "Something kept hidden or unknown to others."
-    },
-    {
         word: "curiosity",
         hint: "A strong desire to know or learn something."
     },
@@ -244,10 +208,6 @@ const wordList = [
         hint: "A false perception or belief; a deceptive appearance or impression."
     },
     {
-        word: "moonlight",
-        hint: "The light from the moon."
-    },
-    {
         word: "vibrant",
         hint: "Full of energy, brightness, and life."
     },
@@ -259,4 +219,132 @@ const wordList = [
         word: "brilliant",
         hint: "Exceptionally clever, talented, or impressive."
     },
+    {
+        word: "equivocal",
+        hint: "Open to more than one interpretation; ambiguous."
+    },
+    {
+        word: "paragon",
+        hint: "A person or thing regarded as a perfect example of a particular quality."
+    },
+    {
+        word: "cactus",
+        hint: "A succulent plant with thick, fleshy stems and spines."
+    },
+    {
+        word: "enigmatic",
+        hint: "Difficult to interpret or understand; mysterious."
+    },
+    {
+        word: "ephemeral",
+        hint: "Lasting for a very short time; fleeting."
+    },
+    {
+        word: "perplexed",
+        hint: "Completely baffled or puzzled."
+    },
+    {
+        word: "profundity",
+        hint: "Deep insight or great depth of knowledge or thought."
+    },
+    {
+        word: "resonate",
+        hint: "Evoke or suggest emotions, memories, or ideas; produce a positive feeling."
+    },
+    {
+        word: "tenacious",
+        hint: "Tending to keep a firm hold of something; persistent."
+    },
+    {
+        word: "eloquence",
+        hint: "Fluent or persuasive speaking or writing."
+    },
+    {
+        word: "ephemeral",
+        hint: "Lasting for a very short time; fleeting."
+    },
+    {
+        word: "ambiguous",
+        hint: "Open to more than one interpretation; having a double meaning."
+    },
+    {
+        word: "Obfuscate",
+        hint: "Render obscure, unclear, or unintelligible."
+    },
+    {
+        word: "Sycophant",
+        hint: "A person who acts obsequiously toward someone important in order to gain advantage."
+    },
+    {
+        word: "Voracious",
+        hint: "Having a very eager approach to a particular activity; having a huge appetite."
+    },
+    {
+        word: "Quintessential",
+        hint: "Representing the most perfect or typical example of a quality or class."
+    },
+    {
+        word: "Nefarious",
+        hint: "Wicked, villainous, or criminal in nature."
+    },
+    {
+        word: "Ebullient",
+        hint: "Overflowing with enthusiasm, excitement, or energy."
+    },
+    {
+        word: "Esoteric",
+        hint: "Intended for or likely to be understood by only a small number of people with a specialized knowledge or interest."
+    },
+    {
+        word: "Cacophony",
+        hint: "A harsh, discordant mixture of sounds."
+    },
+    {
+        word: "Ubiquitous",
+        hint: "Present, appearing, or found everywhere."
+    },
+    {
+        word: "Reticent",
+        hint: "Not revealing one's thoughts or feelings readily."
+    },
+    {
+        word: "Ineffable",
+        hint: "Too great or extreme to be expressed or described in words."
+    },
+    {
+        word: "Surreptitious",
+        hint: "Kept secret, especially because it would not be approved of."
+    },
+    {
+        word: "Disparate",
+        hint: "Essentially different in kind; not allowing comparison."
+    },
+    {
+        word: "Copious",
+        hint: "Abundant in supply or quantity."
+    },
+    {
+        word: "Mellifluous",
+        hint: "Sweet or musical; pleasant to hear."
+    },
+    {
+        word: "Ebullient",
+        hint: "Eager, enthusiastic, and lively."
+    },
+    {
+        word: "Munificent",
+        hint: "More generous than is usual or necessary."
+    },
+    {
+        word: "Sagacious",
+        hint: "Having or showing keen mental discernment and good judgment."
+    },
+    {
+        word: "Vociferous",
+        hint: "Crying out noisily; clamorous."
+    },
+    {
+        word: "Pernicious",
+        hint: "Having a harmful effect, especially in a gradual or subtle way."
+    }
 ];
